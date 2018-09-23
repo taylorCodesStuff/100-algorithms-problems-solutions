63427094a1bcd399f1b4ec3d793b637ef808c517
Taylor Hammons <taylor@thinkful.com>
2018-09-23 17:39:10 -0500

    finished solution to addBorder

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,29 @@
 function addBorder(picture: string[]): string[] {
+    let j = picture[0].length;
+    console.log(j);
+    let newPic = [];
+    for(let k=0;k<picture.length;k++){
+        let cE = picture[k];
+        let newStr = "*" + cE + "*";
+        newPic.push(newStr);
+    }
 
+    console.log(newPic); 
+    let border = "";
+    for(let i=0;i<j+2;i++){
+        let ast = "*";
+        border += ast;
+    }
+    console.log(border);
+
+    newPic.unshift(border);
+    newPic.push(border);
+
+    console.log(newPic);
+    return newPic;
+    
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
+
+// console.log(addBorder(['abcd', 'efgh']));

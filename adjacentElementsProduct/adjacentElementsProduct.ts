ac0c9b54a0fdd7552196880c7e0f0f189b1ae893
Taylor Hammons <taylor@thinkful.com>
2018-09-23 18:40:39 -0500

    finished solution to largestAdjacentProd

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,17 @@
 function adjacentElementsProduct(inputArray: number[]): number {
+    let largestProd = inputArray[0] * inputArray[1];
+    
+    for(let i=1;i<inputArray.length-1;i++){
+        let cE = inputArray[i];
+        let nE = inputArray[i+1];
+        let prod = cE * nE;
+        if(prod > largestProd){
+            largestProd = prod;
+        }
 
+    }
+
+    return largestProd;
 }
 
 console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));

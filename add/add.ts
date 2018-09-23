e042a7790ec0d81fa6128782625230355f410854
Taylor Hammons <taylor@thinkful.com>
2018-09-23 15:33:24 -0500

    finished solution to add

--- add/add.ts
+++ add/add.ts
@@ -2,12 +2,16 @@ function add(param1: number, param2: number): number {
     return param1 + param2;
 }
 
-// function add2(param1: number[]): number {
-
-// }
+function add2(param1: number[]): number {
+    let count = 0;
+    param1.forEach(num => {
+        count += num;
+    });
+    return count;
+}
 console.log(add(1, 2));
 console.log(add(3, 2));
 
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
+console.log(add2([1,2,3,4,5]));
+console.log(add2([2,3]));
 

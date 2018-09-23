cd18200cf0d7085ba7ee287e1995853683c7ba48
Taylor Hammons <taylor@thinkful.com>
2018-09-23 16:45:16 -0500

    finished solution to absSumMin

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,11 @@
 function absoluteValuesSumMinimization(a: number[]): number {
-
+    a.sort();
+    console.log(a);
+    let midNum = Math.floor(a.length/2);
+    return a.length % 2 === 0 ? a[midNum-1] : a[midNum];
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));

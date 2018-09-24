40c4e8a30fe6c01362520184e276aeca02217ce2
Taylor Hammons <taylor@thinkful.com>
2018-09-23 19:37:53 -0500

    finished solution to allLongestStr

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,19 @@
 function allLongestStrings(inputArray: string[]): string[] {
+    let largStrLen = inputArray[0].length;
+    
+    for(let i=1;i<inputArray.length;i++){
+        let currentLength = inputArray[i].length;
+        largStrLen = currentLength > largStrLen ? currentLength : largStrLen;
+    }
 
+    let arr = [];
+    for(i=0;i<inputArray.length;i++){
+        let currElem = inputArray[i];
+        let currElemLen = inputArray[i].length;
+        if(currElemLen === largStrLen) arr.push(currElem);
+    }
+
+    return arr;
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));

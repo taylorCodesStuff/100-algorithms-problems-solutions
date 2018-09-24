bac305c1883cd719aff88c4da1851adfd599d4bb
Taylor Hammons <taylor@thinkful.com>
2018-09-24 16:41:34 -0500

    updated solution to EquallyStrong

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,8 +1,15 @@
 function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+    let yourStrongest = yourLeft >= yourRight ? yourLeft : yourRight;
+    let yourWeakest = yourRight <= yourLeft ? yourRight : yourLeft;
     
-    return yourLeft + yourRight === friendsLeft + friendsRight;
+    let friendsStrongest = friendsLeft >= friendsRight ? friendsLeft : friendsRight;
+    let friendsWeakest = friendsRight <= friendsLeft ? friendsRight : friendsLeft;
+
+    return yourStrongest === friendsStrongest && yourWeakest === friendsWeakest;
 }
 
 console.log(areEquallyStrong(10, 15, 15, 10))
 console.log(areEquallyStrong(15, 10, 15, 10))
 console.log(areEquallyStrong(15, 10, 15, 9))
+
+console.log(areEquallyStrong(15, 10, 24, 1))

7ac8c76f3731485f9e46f54069b81d9c32394f8c
Taylor Hammons <taylor@thinkful.com>
2018-09-23 21:47:12 -0500

    finished solution to  alphabetShift

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,18 @@
 function alphabeticShift(inputString: string): string {
-
+    const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    
+    let strArr = inputString.split('');
+    
+    for(let i=0;i<strArr.length;i++){
+        let cE = strArr[i];
+        if(alphabet.indexOf(cE) === 25) {
+            strArr[i] = 'a';
+        } else {
+            strArr[i] = alphabet[alphabet.indexOf(cE) + 1]; 
+        }
+    }
+    
+    return strArr.join('');
 }
 
 console.log(alphabeticShift('crazy'));

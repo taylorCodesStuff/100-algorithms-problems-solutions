60d5acc74dc2775c61225666414661a1a5f855ef
Taylor Hammons <taylor@thinkful.com>
2018-09-24 21:18:39 -0500

    finished solution to arrayChange

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,18 @@
 function arrayChange(inputArray: number[]): number {
- 
+    let arr = inputArray;
+    let moves = 0;
+    for(let i=0;i<arr.length-1;i++){
+        let currElem = arr[i];
+        let nextElem = arr[i+1];
+        if(nextElem <= currElem){
+            let absDiff = Math.abs(nextElem - currElem);
+            let move = absDiff + 1
+            arr[i+1] += move;
+            moves += move;
+        }
+    }
+
+    return moves;
 }
 
 console.log(arrayChange([1, 1, 1]));

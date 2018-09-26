5eda4611a611806e03d3e31dae4c6975e9feff63
Taylor Hammons <taylor@thinkful.com>
2018-09-25 21:40:55 -0500

    finished solution to arrayReplace

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,12 @@
 function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-   
+    
+    
+    for(let i=0;i<inputArray.length;i++){
+        inputArray[i] = inputArray[i] === elemToReplace ? substitutionElem : inputArray[i];
+    }
+
+
+    return inputArray;
 }
 
 console.log(arrayReplace([1, 2, 1], 1, 3));

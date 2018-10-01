23854822a67b3688f707d42170a533ed5abf669e
Taylor Hammons <taylor@thinkful.com>
2018-09-30 20:22:17 -0500

    finished solution to candies

--- candies/candies.ts
+++ candies/candies.ts
@@ -1,5 +1,18 @@
 function candies(n: number, m: number): number {
-
+    
+    return n * Math.floor(m/n);
 }
 
-console.log(candies(3, 10));
+console.log(candies(3, 10)); // 9
+
+console.log(candies(3, 6)); // 6
+
+console.log(candies(3, 7)); // 6
+
+console.log(candies(2, 7)); // 6
+
+console.log(candies(2, 8)); // 8
+
+console.log(candies(3, 8)); // 6
+
+console.log(candies(4, 10)); // 8

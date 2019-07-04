5ebf610d2122608b5fbd11694afb6e5a1bd177ca
Taylor Hammons <taylor@thinkful.com>
2019-07-03 21:47:56 -0400

    finished solution

--- containsDuplicates/containsDuplicates.ts
+++ containsDuplicates/containsDuplicates.ts
@@ -1,6 +1,20 @@
 function containsDuplicates(a: number[]): boolean {
+    let obj = {};
 
+    for(let i=0;i<a.length;i++){
+        if(!obj[a[i]]) {
+            obj[a[i]] = true;
+        } else return true
+    }
+
+    return false;
 }
 
 console.log(containsDuplicates([1, 2, 3, 1]));
 console.log(containsDuplicates([3, 1]));
+console.log(containsDuplicates([3, 1, 2, 3, 4]));
+console.log(containsDuplicates([1]));
+console.log(containsDuplicates([]));
+console.log(containsDuplicates([9, 8, 2, 5, 1, 2, 5, 3]));
+
+

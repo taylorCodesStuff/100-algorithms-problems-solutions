9f32a37f6acf0cdbb059b807a674728d2c3b3445
Taylor Hammons <taylor@thinkful.com>
2019-07-04 10:10:42 -0500

    finished solution to commonCharCount

--- commonCharacterCount/commonCharacterCount.ts
+++ commonCharacterCount/commonCharacterCount.ts
@@ -1,5 +1,26 @@
 function commonCharacterCount(s1: string, s2: string): number {
-   
+    let count = 0;
+    let s1Arr = s1.split('');
+    let s2Arr = s2.split('');
+    let minObj = {};
+    let maxLenArr = s1Arr.length >= s2Arr.length ? s1Arr : s2Arr;
+    let minLenArr = s1Arr.length <= s2Arr.length ? s1Arr : s2Arr;
+
+
+    for(let i=0;i<minLenArr.length;i++){
+        let cE = minLenArr[i];
+        if(!minObj[cE]) minObj[cE] = true;
+    }
+
+    for(let i=0;i<maxLenArr.length;i++){
+        let cE = maxLenArr[i];
+        if(minObj[cE]) {
+            count++;
+            minObj[cE] = false;
+        }
+    }
+
+    return count;
 }
 
 console.log(commonCharacterCount('aabcc', 'adcaa'));

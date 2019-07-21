6a6e052b92f0a56e13537a0e4ab51376ee51bae3
Taylor Hammons <taylor@thinkful.com>
2019-07-20 23:06:16 -0400

    finished compareInts

--- compareIntegers/compareIntegers.ts
+++ compareIntegers/compareIntegers.ts
@@ -1,5 +1,8 @@
 function compareIntegers(a: string, b: string): string {
+    const numA: number = Number(a); 
+    const numB: number = Number(b);
 
+    return numA > numB ? 'greater' : numA < numB ? 'less' : 'equal';
 }
 
 console.log(compareIntegers('12', '13'));

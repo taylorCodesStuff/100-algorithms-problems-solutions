d15fb4a7fb0ea9822ab7bd4416328a7ddc5c3ec9
Taylor Hammons <taylor@thinkful.com>
2019-07-22 07:53:25 -0400

    working

--- containsCloseNums/containsCloseNums.ts
+++ containsCloseNums/containsCloseNums.ts
@@ -2,5 +2,5 @@ function containsCloseNums(nums: number[], k: number): boolean {
     
 }
 
-console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 3));
-console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 2));
+console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 3)); // true
+console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 2)); // false

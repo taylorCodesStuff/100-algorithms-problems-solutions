94663cedf5066770b3b9116fc01c882f8445339c
Taylor Hammons <taylor@thinkful.com>
2019-08-02 07:35:43 -0400

    comment

--- convertString/convertString.ts
+++ convertString/convertString.ts
@@ -10,16 +10,16 @@ function convertString(s: string, t: string): boolean {
         if(!sHash.hasOwnProperty(cE)) sHash[cE] = 0;
         sHash[cE]++;
     }
-
+    console.log(sHash);
     for(i=0;i<tArr.length;i++){
         let cE = t[i];
         if(sHash[cE]) sHash[cE]--;
 
-        if(sHash[cE] < 0 || !sHash[cE]) return false;
+        if(sHash[cE] < 0) return false;
     }
 
     return true;
 }
-
+// Order matters 
 console.log(convertString('ceoydefthf5iyg5h5yts', 'codefights')); // true 
 console.log(convertString('addbyca', 'abcd')); // false

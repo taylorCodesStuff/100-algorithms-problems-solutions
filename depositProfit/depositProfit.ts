a3866c536916abdd3506bd34045998aa6d99a79c
Taylor Hammons <taylor@thinkful.com>
2019-08-05 07:25:36 -0400

    working

--- depositProfit/depositProfit.ts
+++ depositProfit/depositProfit.ts
@@ -1,5 +1,22 @@
 function depositProfit(deposit: number, rate: number, threshold: number): number {
-    
+    console.log(20/100); 
+    let year = 0;
+    while(deposit < threshold) {
+        deposit += deposit + (deposit * (rate/100));
+        year += 1;
+        console.log(deposit, year);
+    }
+
+    return year;
 }
 
 console.log(depositProfit(100, 20, 170))
+
+
+function calculateGrowth(deposit: number, year: number, threshold: number, rate: number): number {
+    while(deposit < threshold) {
+        deposit += deposit + (deposit * (rate/100));
+        year += 1;
+        console.log(deposit, year);
+    }
+}

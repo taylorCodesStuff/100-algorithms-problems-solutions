8732ae328212eafbb40f8d75bbec6aa0919a27a2
Taylor Hammons <taylor@thinkful.com>
2019-08-07 19:49:08 -0400

    finished

--- differentSymbolsNaive/differentSymbolsNaive.ts
+++ differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,6 +1,18 @@
 function differentSymbolsNaive(s: string): number {
- 
-    
+    let count = 0;
+    let seen = {
+
+    };
+    let charArr = s.split('');
+    for(let i=0;i<charArr.length;i++){
+        let char = charArr[i];
+        if(!seen[char]) {
+            seen[char] = true;
+            count++;
+        }
+    }
+
+    return count;
 }
 
-console.log(differentSymbolsNaive('cabca'));
+console.log(differentSymbolsNaive('cabca')); // 3

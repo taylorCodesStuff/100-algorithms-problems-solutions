97a03054c464c5d45426db9ce28719a71c82b66e
Taylor Hammons <taylor@thinkful.com>
2019-08-06 20:58:15 -0400

    finished

--- crossingSum/crossingSum.ts
+++ crossingSum/crossingSum.ts
@@ -1,6 +1,17 @@
 function crossingSum(matrix: number[][], a: number, b: number): number {
- 
+    let sum = 0;
     
+    for(let i=0;i<matrix[a].length;i++){
+        let j = i;
+        if(i !== b) {
+            sum += matrix[a][i];
+        }
+    }
+
+    for(let j=0;j<matrix.length;j++){
+        sum += matrix[j][b];
+    }
+
 }
 
 console.log(crossingSum([[1, 1, 1, 1], 

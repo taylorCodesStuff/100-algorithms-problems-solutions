59279b32bd6bd4b8fd301fa3f089ab6f2d1d5603
Taylor Hammons <taylor@thinkful.com>
2019-08-06 20:59:36 -0400

    remove unused

--- crossingSum/crossingSum.ts
+++ crossingSum/crossingSum.ts
@@ -2,7 +2,6 @@ function crossingSum(matrix: number[][], a: number, b: number): number {
     let sum = 0;
     
     for(let i=0;i<matrix[a].length;i++){
-        let j = i;
         if(i !== b) {
             sum += matrix[a][i];
         }

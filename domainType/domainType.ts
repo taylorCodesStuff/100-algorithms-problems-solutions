d753bc1b69044f94e58cb6c178b9e45a999cdf4e
Taylor Hammons <taylor@thinkful.com>
2019-09-05 22:13:46 -0400

    finished

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,6 +1,20 @@
 function domainType(domains: string[]): string[] {
-    
-    
+    function typeofDomain(domain: string): string {
+        if (domain.endsWith('.org')) {
+            return 'organization';
+        } else if (domain.endsWith('.com')) {
+            return 'commercial';
+        } else if (domain.endsWith('.net')) {
+            return 'network';
+        } else {
+            return 'information';
+        }
+    }
+
+    return domains.map(url => {
+        return typeofDomain(url);
+    });
 }
 
+// ["organization", "commercial", "network", "information"]
 console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));

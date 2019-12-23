e966e006ebda11fd958c81cb2c18fc2f3e6a8c6e
Taylor Hammons <taylor@thinkful.com>
2019-12-22 22:43:34 -0500

    finished solution

--- evenDigitsOnly/evenDigitsOnly.ts
+++ evenDigitsOnly/evenDigitsOnly.ts
@@ -1,5 +1,13 @@
 function evenDigitsOnly(n: number): boolean {
+    const numStr: string = n.toString();
+    const strArr: string[] = numStr.split('');
+    let isOnlyEven = true;
+    strArr.forEach((str: string) => {
+        const num = parseInt(str);
+        if (num % 2 !== 0) isOnlyEven = false;
+    });
 
+    return isOnlyEven;
 }
 
 console.log(evenDigitsOnly(248622));
